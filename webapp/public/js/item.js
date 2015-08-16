cbc6aadcd2f9b6ebaa9888b7c9db77f7e932f4ef
Ying YIN <6bb79df9@gmail.com>
2015-08-16 15:11:09 +0800

    Tweak CodeMirror integration
    * Use 'solarized dark' theme
    * Set tab size to 2
    * Implement 'w', 'q', and 'wq' editor commands
    * Auto focus editor and quick add input box

--- webapp/public/js/item.js
+++ webapp/public/js/item.js
@@ -93,6 +93,11 @@ angular.module('item', ['ngRoute', 'ui.codemirror'])
   var itemList = this;
   itemList.items = items;
 
+  itemList.title = function(item) {
+    var m = item.content.match(/^\s*([^\r\n]+)/);
+    return m == null ? "<UNTITLED>" : m[0];
+  }
+
   itemList.add = function() {
     Items.create(itemList.content)
       .then(function (data) {
@@ -118,24 +123,69 @@ angular.module('item', ['ngRoute', 'ui.codemirror'])
   }
 })
 
-.controller('ItemEditController', function($location, $routeParams, Items) {
+.controller('ItemEditController', function($scope, $q, $location, $routeParams, Items) {
   var itemEdit = this;
 
   Items.retrieve($routeParams.id)
-      .then(function(data) {
-        itemEdit.item = data;
-      }, function(data) {
-        $location.path('/');
-      });
+    .then(function(data) {
+      itemEdit.item = data;
+    }, function(data) {
+      $location.path('/');
+    });
 
   itemEdit.save = function() {
+    Items.update(itemEdit.item.id, itemEdit.item.content)
+      .then(function (data) {
+      }, function(data) {
+      }, function (data) {
+      });
+  };
+
+  itemEdit.saveAndQuit = function() {
     Items.update(itemEdit.item.id, itemEdit.item.content)
       .then(function (data) {
         $location.path('/');
       }, function(data) {
       }, function (data) {
       });
-  }
+  };
+
+  itemEdit.quit = function() {
+    // TODO: Fix following ugly code for quit current view
+    // The problem is that "$location.path('/')" won't work if quit() is 
+    // called from ':q' Ex command of Vim binding.
+    var deferred = $q.defer();
+    window.setTimeout(function() {
+      deferred.resolve();
+    }, 100);
+
+    deferred.promise.then(function () {
+      $location.path('/');
+    });
+  };
+
+  $scope.cmOptions = {
+    lineWrapping : true,
+    lineNumbers : true,
+    keyMap : 'vim',
+    tabSize : 2,
+    mode : 'text/x-gfm',
+    theme : 'solarized dark',
+    electricChars : false,
+    autofocus : true,
+    onLoad : function(cm) {
+      $scope._cm = cm;
+      CodeMirror.commands.save = function() {
+        itemEdit.save();
+      };
+      CodeMirror.Vim.defineEx("wq", "", function(cm) {
+        itemEdit.saveAndQuit();
+      });
+      CodeMirror.Vim.defineEx("q", "", function(cm) {
+        itemEdit.quit();
+      });
+    }
+  };
 })
 
 ;
